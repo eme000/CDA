9d64db03f02dc356be71ff0e29b72cde9d557378
GabDevMaster <gabriel.commande18@gmail.com>
2024-01-04 15:12:04 +0100

    mini_factory 1

--- 02_Client_leger/WEB_FLASK/app/routes.py
+++ 02_Client_leger/WEB_FLASK/app/routes.py
@@ -1,23 +1,19 @@
+# routes.py
+
 from app import app
 from flask import render_template
 
-
-@app.route('/') # decorators
+@app.route('/')
 def home():
-    return render_template ('home.html')
-    return strResult
+    return render_template('home.html')
 
-@app.route('/mini_factory_1')
-def list():
-    return render_template ('mini_factory_1.html',)
-    return strResult
+@app.route('/mini_factory__1_')
+def mini_factory():
+    return render_template('mini_factory_1.html')
 
-
-@app.route('/list')
-def list():
-    return render_template ('list_bdd.html',)
-    return strResult
-
+@app.route('/list_table')
+def list_table():
+    return render_template('list_bdd.html')
 
 @app.route('/test')
 def show_tables():
@@ -25,4 +21,3 @@ def show_tables():
     table_names = ['Couleur', 'GESTION_ERREUR', 'GESTION_STOCK', 'LUMINOSITE', 'Ordre_de_fabrication', 'PRESSION_ATH', 'PRODUIT', 'TAUX_CO2', 'TAUX_HUMIDITE', 'TEMP_AIR', 'ilo', 'information_erreur', 'suivit_production', 'unité_fab']
 
     return render_template('index.html', liste=table_names)
-
