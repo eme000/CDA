259618d94eafa2a6615d00be91ad0fe5b63d68cf
eme000 <emerannlg@gmail.com>
2024-02-19 16:56:45 +0100

    mdp et acces page web definit

--- 02_Client_leger/WEB_FLASK/app/routes.py
+++ 02_Client_leger/WEB_FLASK/app/routes.py
@@ -6,7 +6,7 @@ import mysql.connector
 from app import inter
 from form import SignUpForm
 
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash,session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -17,12 +17,17 @@ def home():
 
 @app.route('/mini_factory__1_')
 def mini_factory():
-    return render_template('mini_factory_1.html')
+    if 'user' in session:
+        return render_template('mini_factory_1.html')
+    else:
+        flash("Vous devez être connecté pour accéder à cette page.")
+        return redirect(url_for('login'))
 
 @app.route('/mini_factory__1_/mini_factory__1_production')
 def mini_factory_1_production():
    # Passez cette valeur à la page HTML
-    return render_template('mini_factory_1_production.html', last_temperature=inter.getLAST("TEMP_AIR"),
+    if 'user' in session:
+        return render_template('mini_factory_1_production.html', last_temperature=inter.getLAST("TEMP_AIR"),
                            last_co2=inter.getLAST("TAUX_CO2"),
                            last_humidite=inter.getLAST("TAUX_HUMIDITE"),
                            last_luminosite=inter.getLAST("LUMINOSITE"),
@@ -32,18 +37,27 @@ def mini_factory_1_production():
                            nb_blanc=inter.getStock_Couleur(3),
                            date_of=inter.getDate_OF(),
                            couleur_of=inter.getCouleur_OF())
+    else:
+        flash("Vous devez être connecté pour accéder à cette page.")
+        return redirect(url_for('login'))
 
 @app.route('/mini_factory__1_/mini_factory__1_production/info_capteur_env')
 def info_capteur_env_1():
-    return render_template('info_capteur_env.html',last_10_temperature=inter.getLAST10("TEMP_AIR"),
+    if 'user' in session:    
+        return render_template('info_capteur_env.html',last_10_temperature=inter.getLAST10("TEMP_AIR"),
                            last_10_co2=inter.getLAST10("TAUX_CO2"),
                            last_10_humidite=inter.getLAST10("TAUX_HUMIDITE"),
                            last_10_luminosite=inter.getLAST10("LUMINOSITE"),
                            last_10_ath=inter.getLAST10("PRESSION_ATH"))
+    else:
+        flash("Vous devez être connecté pour accéder à cette page.")
+        return redirect(url_for('login'))
+
 
 @app.route('/mini_factory__1_/mini_factory__1_production/Gestion_stock')
 def gestion_stock_1():
-    return render_template('gestion_stock.html',bdd_1_emplcement_1_couleur=inter.getStock("1"),
+    if 'user' in session:   
+        return render_template('gestion_stock.html',bdd_1_emplcement_1_couleur=inter.getStock("1"),
                            bdd_1_emplcement_2_couleur=inter.getStock("2"),
                            bdd_1_emplcement_3_couleur=inter.getStock("3"),
                            bdd_1_emplcement_4_couleur=inter.getStock("4"),
@@ -52,20 +66,24 @@ def gestion_stock_1():
                            bdd_1_emplcement_7_couleur=inter.getStock("7"),
                            bdd_1_emplcement_8_couleur=inter.getStock("8"),
                            bdd_1_emplcement_9_couleur=inter.getStock("9"))
-    
+    else:
+        flash("Vous devez être connecté pour accéder à cette page.")
+        return redirect(url_for('login'))
+
 @app.route('/mini_factory__1_/mini_factory__1_production/OF')
 def info_OF():
     return render_template('info_OF_.html',last_10_of_date=inter.getDate_OF_10(),
                            last_10_of_couleur=inter.getCouleur_OF_10())
 
-@app.route('/list_table')
-def list_table():
-    return render_template('list_bdd.html')
+
 
 @app.route('/mini_factory/Ilots')
 def list_ilots():
-    return render_template('ilots.html')
-
+    if 'user' in session:
+        return render_template('ilots.html')
+    else:
+        flash("Vous devez être connecté pour accéder à cette page.")
+        return redirect(url_for('login'))
 
 @app.route('/test')
 def show_tables():
@@ -105,9 +123,17 @@ def login():
         
         # Appel à la fonction de connexion
         if inter.login_requette(username, hashed_password,password):
+            session['user'] = username
             # Si l'authentification réussit, redirigez l'utilisateur vers la page d'accueil
             return redirect(url_for('home'))
         else:
             # Sinon, affichez un message d'erreur ou redirigez vers une page d'erreur
             flash("Nom d'utilisateur ou mot de passe incorrect.")
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', default=None)
+    print ("logout")
+    return redirect(url_for('home'))
